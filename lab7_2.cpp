8ad425354c892dc6ff1ee5bf04d03d30269e8cf8
Noppanat <pangpondzaza.1604@gmail.com>
2025-12-12 16:07:05 +0700

    finish

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,9 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    
+    string id;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> id;
+    cin.ignore();
+    int gear = (id[0]- 48)*10 + (id[1]-48);
+    cout << "Fahsai: I think you may be GEAR " << gear-12 << ". I have a free movie ticket for you.\n";
+    
+    string movie;
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ": ";
+    getline(cin,movie);
+
+    string day;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";
+    getline(cin,day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+
+    string text;
+    cout << name << ": ";
+    getline(cin,text);
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+
+    return 0;
+}
 //Dialog of the first example is given below.
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
 Fahsai: Wow!!! Luke Skywalker is a really cool name.
+
 Fahsai: I think you are an Engineering student. What is your student ID?
 Luke Skywalker: 590610999
 Fahsai: I think you may be GEAR 47. I have a free movie ticket for you.
